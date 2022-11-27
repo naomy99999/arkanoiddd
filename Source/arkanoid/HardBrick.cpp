d03f5d0917b14beafb63b7e4bb3ad2f99a9e06b9
Naomy Yailín Paredes Paredes <114457380+naomy99999@users.noreply.github.com>
2022-11-26 18:43:15 -0600

    añadiendo Hardbrick

--- Source/arkanoid/HardBrick.cpp
+++ Source/arkanoid/HardBrick.cpp
@@ -1,5 +1,30 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "HardBrick.h"
+#include "Ball.h"
 
+
+
+AHardBrick::AHardBrick()
+{
+}
+
+void AHardBrick::DestroyBrick()
+{
+
+}
+void AHardBrick::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndexType, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag("Ball")) {
+
+		ABall* MyBall = Cast<ABall>(OtherActor);
+
+		FVector BallVelocity = MyBall->GetVelocity();
+		BallVelocity *= (SpeedModifierOnBounce - 1.0f);
+
+		MyBall->GetBall()->SetPhysicsLinearVelocity(BallVelocity, true);
+
+		FTimerHandle UnusedHandle;
+		GetWorldTimerManager().SetTimer(UnusedHandle, this, &AHardBrick::DestroyBrick, 0.1f, false);
+	}
+}
