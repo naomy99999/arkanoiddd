55ad2ef2105cf1113ce4ce80374a728f066cd744
Naomy Yailín Paredes Paredes <114457380+naomy99999@users.noreply.github.com>
2022-11-26 19:08:32 -0600

    añadiendo Queen

--- Source/arkanoid/Queen.h
+++ Source/arkanoid/Queen.h
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,17 +9,30 @@ UCLASS()
 class ARKANOID_API AQueen : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AQueen();
+
+private:
+	FString brazo;
+	FString rostro;
+	FString pierna;
+	float movimientoX, movimientoY;
+	FString proyectil;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void setBrazo(FString myBrazo);
+	void setRostro(FString myRostro);
+	void setPierna(FString myPierna);
+	void setMovimientoX(float myMovimientoX);
+	void setMovimientoY(float myMovimientoY);
+	void setProyectil(FString myProyectil);
 };
